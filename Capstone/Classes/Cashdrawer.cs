663d14f80919a0bfef69c6cccc447f7d412a00b1
Justin Gompers <justinmgompers@aol.com>
2019-02-07 13:18:41 -0500

    fixed

--- Capstone/Classes/Cashdrawer.cs
+++ Capstone/Classes/Cashdrawer.cs
@@ -6,5 +6,36 @@ namespace Capstone.Classes
 {
     class Cashdrawer
     {
+        protected decimal amountInMachine { get; private set; }
+        protected decimal amountWithTransaction { get; set; }
+        protected decimal amountInserted { get; set; }
+
+        Cashdrawer()
+        {
+            amountInMachine = 0;
+            amountInserted = 0;
+
+        }
+
+        public void Deposit(decimal depositAmount)
+        {
+            amountInserted = depositAmount;
+            amountInMachine += amountInserted;
+            Console.WriteLine($"{amountInserted} : {amountInMachine}");
+
+        }
+
+        public void Withraw(decimal productPrice)
+        {
+            amountWithTransaction = amountInMachine;
+            amountWithTransaction -= productPrice;
+            Console.WriteLine($"{amountInMachine} : {amountWithTransaction}");
+            amountInMachine -= amountWithTransaction;
+        }
+
+        public void TransactionLog(decimal beforeTansaction, decimal afterTransaction)
+        {
+
+        }
     }
 }
